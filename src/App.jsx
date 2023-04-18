74d9b27b57d23efc26a0115126ca03ce7105a7bc
ScreenBottle <arvid.rydh@iths.se>
2023-04-18 13:34:08 +0200

    added firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { initializeApp } from "firebase/app";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA63z_hiERVvdwqjaFzotvDSv4i2khRX4c",
+    authDomain: "au22cinema-planet.firebaseapp.com",
+    projectId: "au22cinema-planet",
+    storageBucket: "au22cinema-planet.appspot.com",
+    messagingSenderId: "271914838545",
+    appId: "1:271914838545:web:4851c4d8fc1a38eaa5dfb0"
+  };
+
+  const [app, setApp] = useState(null)
+
+  useEffect(() => {
+    setApp(initializeApp(firebaseConfig));
+  }, [])
 
   return (
     <div className="App">
