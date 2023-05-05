7d773b066ff624a6786861dbb52f8eefcc4a309e
alex88g <alexander.gallorini@gmail.com>
2023-05-05 10:25:26 +0200

    web deployed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/AU22Cinema-Planet/',
+  base: 'AU22Cinema-Planet',
   plugins: [react()],
 })
